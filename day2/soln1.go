b0ff07a6972ba7da00c3748b643b39e01d6ec025
Quinn McCarthy Beaver <quinn.mccarthybeaver@gmail.com>
2022-12-07 16:41:52 -0500

    day2

---
+++ day2/soln1.go
@@ -0,0 +1,114 @@
+package day2
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+)
+
+func Soln1(reader *bufio.Scanner) int {
+	var lefts []string
+	var rights []string
+	for reader.Scan() {
+		in := strings.Fields(reader.Text())
+		lefts = append(lefts, in[0])
+		rights = append(rights, in[1])
+	}
+	var score int
+	for idx, val := range lefts {
+		victory := 3
+		switch rights[idx] {
+		case "X":
+			score += 1
+			if val == "B" {
+				victory -= 3
+			} else if val == "C" {
+				victory += 3
+			}
+		case "Y":
+			score += 2
+			if val == "C" {
+				victory -= 3
+			} else if val == "A" {
+				victory += 3
+			}
+		case "Z":
+			score += 3
+			if val == "A" {
+				victory -= 3
+			} else if val == "B" {
+				victory += 3
+			}
+		}
+		score += victory
+	}
+	fmt.Println(score)
+	return score
+}
+
+func Soln2(reader *bufio.Scanner) int {
+	var lefts []string
+	var rights []string
+	for reader.Scan() {
+		in := strings.Fields(reader.Text())
+		lefts = append(lefts, in[0])
+		rights = append(rights, in[1])
+	}
+	var score int
+	for idx, val := range lefts {
+		switch rights[idx] {
+		case "X":
+			score += lose_score(val)
+		case "Y":
+			score += 3 + tie_score(val)
+		case "Z":
+			score += 6 + win_score(val)
+		}
+	}
+	fmt.Println(score)
+	return score
+}
+
+// A B C -> Rock Paper Scisors
+// 1 2 3
+// win: 6
+// tie: 3
+// los; 0
+func lose_score(x string) int {
+	switch x {
+	case "A":
+		return 3
+	case "B":
+		return 1
+	case "C":
+		return 2
+	default:
+		return 0
+	}
+}
+
+func win_score(x string) int {
+	switch x {
+	case "A":
+		return 2
+	case "B":
+		return 3
+	case "C":
+		return 1
+	default:
+		return 0
+	}
+}
+
+func tie_score(x string) int {
+	switch x {
+	case "A":
+		return 1
+	case "B":
+		return 2
+	case "C":
+		return 3
+	default:
+		return 0
+	}
+}
--- main.go
+++ main.go
@@ -2,11 +2,33 @@ package main
 
 import (
 	"22advent/day1"
+	"22advent/day2"
 	"bufio"
+	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-	day1.Soln1(scanner)
+	if len(os.Args) != 2 {
+		fmt.Println("give a single number as an arg")
+		return
+	}
+
+	var day int
+	var err error
+	day, err = strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	switch day {
+	case 1:
+		day1.Soln1(scanner)
+	case 2:
+		day2.Soln2(scanner)
+	default:
+		fmt.Println("not implemented yet")
+	}
 }
